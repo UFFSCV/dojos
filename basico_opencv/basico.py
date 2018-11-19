f9052189a8c90a3704cdd8c7d890d5220d4616d9
@oraphaborges <rah-borges@hotmail.com>
2018-11-19 18:35:55 -0200

    dojo de básico de opencv

---
+++ basico_opencv/basico.py
@@ -0,0 +1,33 @@
+# Importar a bibliotéca do OpenCV
+import cv2
+
+# Importar uma imagem
+img_COR =
+img_PB  =
+
+# Salvar uma imagem
+
+
+# Plotar com OpenCV
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Rotação
+linhas,colunas =
+
+angulo =
+centro_de_rotacao = (colunas/2,linhas/2)
+escala =
+
+rotacao =
+rotacionado =
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Inter imagem vertical e horizontal
+inverte_horizontal =
+inverte_vertical =
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
