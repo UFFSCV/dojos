bc9df1b1074927d500e48a342d08769f31665333
Raphael Borges <rah-borges@hotmail.com>
2019-03-11 14:30:55 -0300

    Criado Dojo SeamlessCloning

---
+++ SeamlessCloning/SeamlessCloning.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+# Leitura das Imagens
+src = None
+dst = None
+
+# Criando a mascara do obejeto
+mask = None
+
+# Definindo local do objeto
+place = None
+
+# Clone seamlessly.
+output_normal = None
+output_mixed = None
+output_monochrome_transfer = None
+
+# Mostar os resultados
