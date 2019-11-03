7a1a56f9182a5c72b95ddb348d2e3c643da294f8
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2019-11-03 17:51:32 -0300

    updates kmeans dojo

--- ImageSegmentation/kmeans.py
+++ ImageSegmentation/kmeans.py
@@ -8,15 +8,15 @@ Z = img.reshape((-1, 3))
 Z = np.float32(Z)
 
 # Criterio de parada, numero de iterações e precisão
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 5
-ret, label, center = cv.kmeans(Z, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+criteria =
+K =
+ret, label, center =
 
 # Ajustar o tipo da resposta para o imshow
 center = np.uint8(center)
 res = center[label.flatten()]
 res2 = res.reshape((img.shape))
 
-cv.imshow('res2',res2)
+cv.imshow("K-means Image",res2)
 cv.waitKey(0)
 cv.destroyAllWindows()
